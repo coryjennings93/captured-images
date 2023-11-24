62cafa4c2a6a39fdc9a062ce522c32719660ad3c
Cory Jennings <corydjennings@gmail.com>
2023-11-23 23:52:44 -0500

    added components and installed shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    // BrowserRouter will controll the routing for the entire application
+    // used to import { Routes, Route } from 'react-router-dom' in App.tsx allowing everything to be wrapped in a <main> section in <App>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
